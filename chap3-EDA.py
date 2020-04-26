240e9f4dd5be5f81e4cc6f06220af31c013a12da
septem9907 <septem9907@gmail.com>
2020-04-26 10:33:55 +0800

    update the file for chap3, fix typo

--- chap3-EDA.py
+++ chap3-EDA.py
@@ -26,3 +26,40 @@ plt.ylabel("Cumulative Percent")
 plt.show()
 
 # %%
+result = (
+    mortgage.groupby(["default_time"], as_index=False)
+        .id.count()
+        .rename(columns={"id": "frequency"})
+        .assign(percent=lambda x: x.frequency / x.frequency.sum() * 100)
+)
+print(result)
+
+# %%
+# Location Measure
+
+# %%
+loc_measures = (
+    mortgage.loc[:, ["default_time", "FICO_orig_time", "LTV_orig_time"]]
+        .describe()
+        .transpose()
+)
+print(loc_measures)
+
+# %%
+from statsmodels.graphics.gofplots import qqplot
+qqplot(mortgage.FICO_orig_time)
+
+# %%
+import seaborn as sns
+sns.set(style="whitegrid")
+
+# %%
+sns.boxplot(x="default_time", y="FICO_orig_time", data=mortgage)
+
+# %%
+(
+    mortgage.groupby(["default_time"])
+        .boxplot(column=["FICO_orig_time"])
+)
+
+# %%
